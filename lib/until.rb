85e20ae6e6bdcb2550cd4576cd46f932c6eff6e4
abstract-animation-9914 <vm930@hotmail.com>
2019-01-01 21:31:11 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,8 @@
 def using_until
         levitation_force = 6
         until levitation_force == 0
-        do levitation_force = levitation_force -1
-        break
+        puts "Wingardium Leviosa"
+        levitation_force = levitation_force -1
+      end
 end
 
